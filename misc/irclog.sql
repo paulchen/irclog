c487ea9c86c75910131e915f1a41d9d9d26e2664
paulchen <paulchen@rueckgr.at>
2016-02-16 16:27:40 +0100

    added table 'user'

--- misc/irclog.sql
+++ misc/irclog.sql
@@ -1,13 +1,21 @@
+CREATE TABLE "user" (
+    user_pk serial NOT NULL,
+    username text NOT NULL,
+    color varchar(6) NOT NULL,
+    PRIMARY KEY (user_pk)
+);
+
 CREATE TABLE message (
     message_pk serial NOT NULL,
     source_file character varying(14) NOT NULL,
     line integer NOT NULL,
     "timestamp" timestamp without time zone NOT NULL,
-    nickname text NOT NULL,
+    user_fk integer,
     raw_text text NOT NULL,
     text text not null,
     deleted bool NOT NULL DEFAULT false,
-    primary key(message_pk)
+    primary key (message_pk),
+    foreign key (user_fk) references "user" (user_pk)
 );
 
 CREATE TABLE settings (
